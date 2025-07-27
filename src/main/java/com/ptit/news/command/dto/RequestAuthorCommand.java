88f094cfa5dcb5f21e2b35832c4b9bd229fb424e
HuyALT <tiendat.photo105@gmail.com>
2025-07-27 21:38:33 +0700

    request author
    New ENUM Request Author status

---
+++ src/main/java/com/ptit/news/command/dto/RequestAuthorCommand.java
@@ -0,0 +1,16 @@
+package com.ptit.news.command.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestAuthorCommand {
+    private String profileUrl;
+    private String sampleArticles;
+    private String reason;
+}
--- src/main/java/com/ptit/news/command/handler/CreateNewsCommandHandler.java
+++ src/main/java/com/ptit/news/command/handler/CreateNewsCommandHandler.java
@@ -3,16 +3,20 @@ package com.ptit.news.command.handler;
 import com.ptit.news.command.dto.CreateNewsCommand;
 import com.ptit.news.command.dto.CreateUserCommand;
 import com.ptit.news.common.Response;
+import com.ptit.news.common.enums.UserRole;
 import com.ptit.news.dto.NewsResponse;
 import com.ptit.news.entity.Category;
 import com.ptit.news.entity.News;
+import com.ptit.news.entity.Role;
 import com.ptit.news.entity.User;
 import com.ptit.news.repository.CategoryRepository;
 import com.ptit.news.repository.NewsRepository;
+import com.ptit.news.repository.RoleRepository;
 import com.ptit.news.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.CommandHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -30,19 +34,26 @@ public class CreateNewsCommandHandler {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     @CommandHandler
     public Response<NewsResponse> handle(CreateNewsCommand command) {
         try {
-            // Có auth
-//            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//            User user = userRepository.findByEmail(authentication.getName()).orElse(null);
-//
+
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            User user = userRepository.findByEmail(authentication.getName()).orElse(null);
 
             // test
-            User user = userRepository.findByEmail("writer@gmail.com").orElse(null);
+//            User user = userRepository.findByEmail("writer@gmail.com").orElse(null);
+
             if (user == null) {
                 return Response.Error("Không tìm thấy authencation");
             }
+//            Role writerRole = roleRepository.findByName("WRITER").orElse(null);
+//            if (!user.getRoles().contains(writerRole)){
+//                throw new AccessDeniedException("không có quyền đăng bài");
+//            }
             Category category = categoryRepository.findById(command.getCategoryId()).orElse(null);
             if (category == null) {
                 return Response.Error("Chủ đề không tồn tại");
